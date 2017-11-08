eec1108ee2b151ca60a8a2d26de0092d9e875835
Alina Tudose <alina.tudose@travelbird.com>
2017-11-08 20:51:27 +0100

    add functionality to search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import BusinessList from './components/BusinessList/BusinessList.js'
@@ -20,15 +19,20 @@ const business = {
 const businesses = [business, business, business, business, business, business]
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <h1>ravenous</h1>
-            <SearchBar />
-            <BusinessList businesses={businesses}/>
-        </div>
-    );
-  }
+
+    searchYelp(term, location, sortBy) {
+        console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>ravenous</h1>
+                <SearchBar searchYelp={this.searchYelp}/>
+                <BusinessList businesses={businesses}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,8 +7,8 @@ class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                {this.props.businesses.map(business => {
-                    <Business business={business} />
+                {this.props.businesses.map((business, i) => {
+                    return <Business key={`business${i}`} business={business} />
                 })}
             </div>
         )
